167a72fea1c0a6a7b784840e23befff6416ad78d
rayyagenaro <147696479+rayyagenaro@users.noreply.github.com>
2023-12-08 14:23:51 +0700

    Update GameMain.java
    ganti font arial

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,129 +1,75 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.*;
+import javax.swing.border.Border;
+
 /**
- * Tic-Tac-Toe: Two-player Graphic version with better OO design.
- * The Board and Cell classes are separated in their own classes.
+ * The Cell class model the cells of the Sudoku puzzle, by customizing (subclass)
+ * the javax.swing.JTextField to include row/column, puzzle number and status.
  */
-public class GameMain extends JPanel {
-    private static final long serialVersionUID = 1L; // to prevent serializable warning
+public class Cell extends JTextField {
+    private static final long serialVersionUID = 1L;  // to prevent serial warning
 
-    // Define named constants for the drawing graphics
-    public static final String TITLE = "Tic Tac Toe";
-    public static final Color COLOR_BG = Color.WHITE;
-    public static final Color COLOR_BG_STATUS = new Color(216, 216, 216);
-    public static final Color COLOR_CROSS = new Color(239, 105, 80);  // Red #EF6950
-    public static final Color COLOR_NOUGHT = new Color(64, 154, 225); // Blue #409AE1
-    public static final Font FONT_STATUS = new Font("OCR A Extended", Font.PLAIN, 14);
+    // Define named constants for JTextField's colors and fonts
+    //  to be chosen based on CellStatus
+    public static final Color BG_GIVEN = new Color(240, 240, 240); // RGB
+    public static final Color FG_GIVEN = Color.BLACK;
+    public static final Color FG_NOT_GIVEN = Color.GRAY;
+    public static final Color BG_TO_GUESS  = Color.WHITE;
+    public static final Color BG_CORRECT_GUESS = new Color(0, 216, 0);
+    public static final Color BG_WRONG_GUESS   = new Color(216, 0, 0);
+    public static final Font FONT_NUMBERS = new Font("Courier New", Font.PLAIN, 28);
 
-    // Define game objects
-    private Board board;         // the game board
-    private State currentState;  // the current state of the game
-    private Seed currentPlayer;  // the current player
-    private JLabel statusBar;    // for displaying status message
 
-    /** Constructor to setup the UI and game components */
-    public GameMain() {
 
-        // This JPanel fires MouseEvent
-        super.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {  // mouse-clicked handler
-                int mouseX = e.getX();
-                int mouseY = e.getY();
-                // Get the row and column clicked
-                int row = mouseY / Cell.SIZE;
-                int col = mouseX / Cell.SIZE;
 
-                if (currentState == State.PLAYING) {
-                    if (row >= 0 && row < Board.ROWS && col >= 0 && col < Board.COLS
-                            && board.cells[row][col].content == Seed.NO_SEED) {
-                        // Update cells[][] and return the new game state after the move
-                        currentState = board.stepGame(currentPlayer, row, col);
-                        // Switch player
-                        currentPlayer = (currentPlayer == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
-                    }
-                } else {        // game over
-                    newGame();  // restart the game
-                }
-                // Refresh the drawing canvas
-                repaint();  // Callback paintComponent().
-            }
-        });
+    // Define properties (package-visible)
+    /** The row and column number [0-8] of this cell */
+    int row, col;
+    /** The puzzle number [1-9] for this cell */
+    int number;
+    /** The status of this cell defined in enum CellStatus */
+    CellStatus status;
 
-        // Setup the status bar (JLabel) to display status message
-        statusBar = new JLabel();
-        statusBar.setFont(FONT_STATUS);
-        statusBar.setBackground(COLOR_BG_STATUS);
-        statusBar.setOpaque(true);
-        statusBar.setPreferredSize(new Dimension(300, 30));
-        statusBar.setHorizontalAlignment(JLabel.LEFT);
-        statusBar.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 12));
-
-        super.setLayout(new BorderLayout());
-        super.add(statusBar, BorderLayout.PAGE_END); // same as SOUTH
-        super.setPreferredSize(new Dimension(Board.CANVAS_WIDTH, Board.CANVAS_HEIGHT + 30));
-        // account for statusBar in height
-        super.setBorder(BorderFactory.createLineBorder(COLOR_BG_STATUS, 2, false));
-
-        // Set up Game
-        initGame();
-        newGame();
+    /** Constructor */
+    public Cell(int row, int col) {
+        super();   // JTextField
+        this.row = row;
+        this.col = col;
+        // Inherited from JTextField: Beautify all the cells once for all
+        super.setHorizontalAlignment(JTextField.CENTER);
+        super.setFont(FONT_NUMBERS);
     }
 
-    /** Initialize the game (run once) */
-    public void initGame() {
-        board = new Board();  // allocate the game-board
+    /** Reset this cell for a new game, given the puzzle number and isGiven */
+    public void newGame(int number, boolean isGiven) {
+        this.number = number;
+        status = isGiven ? CellStatus.GIVEN : CellStatus.TO_GUESS;
+        paint();    // paint itself
     }
 
-    /** Reset the game-board contents and the current-state, ready for new game */
-    public void newGame() {
-        for (int row = 0; row < Board.ROWS; ++row) {
-            for (int col = 0; col < Board.COLS; ++col) {
-                board.cells[row][col].content = Seed.NO_SEED; // all cells empty
-            }
+    /** This Cell (JTextField) paints itself based on its status */
+    public void paint() {
+        if (status == CellStatus.GIVEN) {
+            // Inherited from JTextField: Set display properties
+            super.setText(number + "");
+            super.setEditable(false);
+            super.setBackground(BG_GIVEN);
+            super.setForeground(FG_GIVEN);
+        } else if (status == CellStatus.TO_GUESS) {
+            // Inherited from JTextField: Set display properties
+            super.setText("");
+            super.setEditable(true);
+            super.setBackground(BG_TO_GUESS);
+            super.setForeground(FG_NOT_GIVEN);
+        } else if (status == CellStatus.CORRECT_GUESS) {  // from TO_GUESS
+            super.setBackground(BG_CORRECT_GUESS);
+        } else if (status == CellStatus.WRONG_GUESS) {    // from TO_GUESS
+            super.setBackground(BG_WRONG_GUESS);
         }
-        currentPlayer = Seed.CROSS;    // cross plays first
-        currentState = State.PLAYING;  // ready to play
+
+
     }
 
-    /** Custom painting codes on this JPanel */
-    @Override
-    public void paintComponent(Graphics g) {  // Callback via repaint()
-        super.paintComponent(g);
-        setBackground(COLOR_BG); // set its background color
 
-        board.paint(g);  // ask the game board to paint itself
-
-        // Print status-bar message
-        if (currentState == State.PLAYING) {
-            statusBar.setForeground(Color.BLACK);
-            statusBar.setText((currentPlayer == Seed.CROSS) ? "Cow's Turn" : "Owl's Turn");
-        } else if (currentState == State.DRAW) {
-            statusBar.setForeground(Color.RED);
-            statusBar.setText("It's a Draw! Click to play again.");
-        } else if (currentState == State.CROSS_WON) {
-            statusBar.setForeground(Color.RED);
-            statusBar.setText("'Cow' Won! Click to play again.");
-        } else if (currentState == State.NOUGHT_WON) {
-            statusBar.setForeground(Color.RED);
-            statusBar.setText("'Owl' Won! Click to play again.");
-        }
-    }
-
-    /** The entry "main" method */
-    public static void main(String[] args) {
-        // Run GUI construction codes in Event-Dispatching thread for thread safety
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame frame = new JFrame(TITLE);
-                // Set the content-pane of the JFrame to an instance of main JPanel
-                frame.setContentPane(new GameMain());
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setLocationRelativeTo(null); // center the application window
-                frame.setVisible(true);            // show it
-            }
-        });
-    }
 }
